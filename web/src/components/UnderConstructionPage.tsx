522fcec25ff1b3b2ca8ac8853ec37a3be1a27b87
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-05-22 16:17:55 +0200

    animation working

--- web/src/components/UnderConstructionPage.tsx
+++ web/src/components/UnderConstructionPage.tsx
@@ -4,11 +4,11 @@ export default function UnderConstructionPage() {
     return (
     <>
         <span className='Under-construction-page'>
-                <span className="text-4xl font-bold mb-4">Under Construction</span>
+                <span className="text-4xl font-bold mb-4 animate-fadeIn">Under Construction</span>
                 <span className='Main-Image'>
                     <img alt='cloud_banner' src='under_construction.svg' />
                 </span>
-                <span className="flex text-2xl mb-4">
+                <span className="flex text-2xl mb-4 animate-fadeIn">
                     The Nolo Team is hard at work building Project Name website <br />
                     Updates will follow soon. </span>
         </span>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-3deg)' },
+                    '50%': { transform: 'rotate(3deg)' },
+                },
+                fadeIn: {
+                    '0%, 100%': { opacity: '100%' },
+                    '0%': { opacity: '0%' },
+                }
+            },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+                fadeIn: 'fadeIn 2s ease-in',
+            }
+        },
     },
     plugins: [],
 }
