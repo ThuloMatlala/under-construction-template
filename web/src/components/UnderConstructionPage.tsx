ab1072f12ffdc114ce1bad8f7bd992fec7b3483e
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-05-22 15:46:57 +0200

    Create basic construction page

---
+++ web/src/components/UnderConstructionPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function UnderConstructionPage() {
+    return (
+    <>
+        <span className='Under-construction-page'>
+            <span className="text-4xl font-bold mb-4">Under Construction</span>
+        </span>
+    </>
+)
+}
