ddd0981b4f6741e9c9add20ab10db398bf567f48
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-05-22 16:00:59 +0200

    wip. We are getting there

--- web/src/components/UnderConstructionPage.tsx
+++ web/src/components/UnderConstructionPage.tsx
@@ -5,8 +5,11 @@ export default function UnderConstructionPage() {
     <>
         <span className='Under-construction-page'>
                 <span className="text-4xl font-bold mb-4">Under Construction</span>
+                <span className='Main-Image'>
+                    <img alt='cloud_banner' src='under_construction.svg' />
+                </span>
                 <span className="flex text-2xl mb-4">
-                    The Nolo Team is hard at work building Dr. Jeanett Maepa's website <br />
+                    The Nolo Team is hard at work building Project Name website <br />
                     Updates will follow soon. </span>
         </span>
     </>
