2d56049acb9583a463ca01e5bca01e247e516782
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-05-22 15:51:04 +0200

    Add minor copy

--- web/src/components/UnderConstructionPage.tsx
+++ web/src/components/UnderConstructionPage.tsx
@@ -4,7 +4,10 @@ export default function UnderConstructionPage() {
     return (
     <>
         <span className='Under-construction-page'>
-            <span className="text-4xl font-bold mb-4">Under Construction</span>
+                <span className="text-4xl font-bold mb-4">Under Construction</span>
+                <span className="flex text-2xl mb-4">
+                    The Nolo Team is hard at work building Dr. Jeanett Maepa's website <br />
+                    Updates will follow soon. </span>
         </span>
     </>
 )
